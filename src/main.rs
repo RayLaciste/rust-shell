be81ff49390525bfd13bea36a3fef4d2c4e3b841
RayLaciste <67614706+RayLaciste@users.noreply.github.com>
2026-04-30 15:23:15 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,6 @@ fn main() {
     io::stdout().flush().unwrap();
     
     let command = String::new();
-    io::stdout().read_line(&mut command).unwrap();
+    io::stdin().read_line(&mut command).unwrap();
     println!("{}: command not found", command.trim());
 }
