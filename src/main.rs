afff7fcecd00b04cd010177dfa9b38b30eeb26fc
RayLaciste <67614706+RayLaciste@users.noreply.github.com>
2026-05-06 14:47:48 -0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,94 +1,77 @@
-use std::env;
 #[allow(unused_imports)]
+use std::collections::HashMap;
+use std::env;
+use std::fs;
 use std::io::{self, Write};
 use std::os::unix::fs::PermissionsExt;
 use std::os::unix::process::CommandExt;
 use std::path::Path;
 
 fn main() {
+    let mut executables = HashMap::new();
+    let path = std::env::var("PATH").unwrap();
+    for dir in path.split(':').filter_map(|d| fs::read_dir(d).ok()) {
+        for entry in dir {
+            let entry = entry.unwrap();
+            let file_name = entry.file_name().into_string().unwrap();
+            let path = entry.path();
+            if let Ok(metadata) = fs::metadata(&path) 
+                && metadata.is_file() 
+                && metadata.permissions().mode() & 0o111 != 0 
+                && !executables.contains_key(&file_name){
+                executables.insert(file_name, path);
+            }
+        }
+    }
+
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
-        let mut command: String = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        let command: &str = command.trim(); // command + args
+        let mut input: String = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading line");
+
+        let input = input.split_whitespace().collect::<Vec<&str>>();
+        let command = input[0];
+        let arguments: &[&str] = &input[1..];
 
         let path_env = env::var("PATH").unwrap_or_default();
-        let builtin = ["echo", "type", "exit"];
+        let builtin = ["echo", "type", "exit", "pwd"];
 
-        // ----- EXIT COMMAND
-        if command == "exit" {
-            break;
-        }
-
-        // ----- ECHO COMMAND
-        if let Some(rest) = command.strip_prefix("echo ") {
-            println!("{}", rest);
-            continue;
-        }
-
-        // ----- TYPE COMMANAND
-        if let Some(rest) = command.strip_prefix("type ") {
-            let args: &str = rest;
-            if builtin.contains(&args) {
-                println!("{} is a shell builtin", args);
-            } else {
-                let found = path_env.split(':').find_map(|dir| {
-                    let full_path = format!("{}/{}", dir, args);
-                    if Path::new(&full_path).is_file() {
-                        // does the file exist
-                        let permissions = std::fs::metadata(&full_path).unwrap().permissions();
-                        // mode gets the permission bits
-                        // 0o111 in octal is 001 001 001
-                        // ( r w x | r w x | r w x) read write execute
-                        if permissions.mode() & 0o111 != 0 {
-                            // is the file executable
-                            Some(full_path)
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                });
-                match found {
-                    Some(path) => println!("{} is {}", args, path),
-                    None => println!("{}: not found", args),
-                }
+        match command {
+            "echo" => {
+                println!("{}", arguments.join(" "));
             }
-
-            continue;
-        }
-
-        // ----- EXECUTE PROGRAM
-        let parts: Vec<&str> = command.split_whitespace().collect();
-        if let Some((&program, args)) = parts.split_first() {
-            let found = path_env.split(':').find_map(|dir| {
-                let full_path = format!("{}/{}", dir, program);
-                if Path::new(&full_path).is_file() {
-                    let permissions = std::fs::metadata(&full_path).unwrap().permissions();
-                    if permissions.mode() & 0o111 != 0 {
-                        Some(full_path)
+            // ----- EXIT COMMAND
+            "exit" => {
+                break;
+            }
+            // ----- PWD COMMAND
+            "pwd" => { println!("{}", env::current_dir().unwrap().display()); }
+            "type" => match arguments[0] {
+                arg @ ("echo" | "exit" | "pwd" | "type") => println!("{} is a shell builting", arg),
+                arg => {
+                    if let Some(path) = executables.get(arg) {
+                        println!("{} is {}", arg, path.to_str().to_owned().unwrap());
                     } else {
-                        None
+                        println!("{}: not found", arg);
                     }
+                }
+            },
+            command => {
+                if let Some(path) = executables.get(command) {
+                    let mut process = std::process::Command::new(path)
+                        .arg0(command)
+                        .args(arguments)
+                        .spawn()
+                        .unwrap();
+                    process.wait().unwrap();
                 } else {
-                    None
+                    println!("{}: command not found", command);
                 }
-            });
-
-            if let Some(path) = found {
-                std::process::Command::new(&path)
-                    .arg0(program)
-                    .args(args)
-                    .status()
-                    .unwrap();
-                continue;
             }
         }
-
-        // ----- default
-        println!("{}: command not found", command);
     }
 }
